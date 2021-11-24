79965249597d310892d550b25269d14933e12d14
Scorpionsik <36457365+Scorpionsik@users.noreply.github.com>
2021-11-24 10:27:31 +0200

    Start with git

---
+++ src/test/java/io/github/scorpionsik/learn/LearnSpringInAction/LearnSpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.scorpionsik.learn.LearnSpringInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
