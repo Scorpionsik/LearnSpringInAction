3f38ec2dfee894c50fa95153686b35be5e2bc8ba
Scorpionsik <36457365+Scorpionsik@users.noreply.github.com>
2021-11-28 18:25:49 +0200

    Replace default controller with view controller config

---
+++ src/main/java/io/github/scorpionsik/learn/LearnSpringInAction/web/WebConfig.java
@@ -0,0 +1,17 @@
+package io.github.scorpionsik.learn.LearnSpringInAction.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Declare a view controllers that does nothing but forward the request to a view
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+    }
+}
