5f82c7371280087d684a886f2ad1f03b7fe5c3ab
Scorpionsik <36457365+Scorpionsik@users.noreply.github.com>
2021-12-24 15:44:38 +0200

    Add repository for ingredients

---
+++ src/main/java/io/github/scorpionsik/learn/LearnSpringInAction/repositiries/IngredientRepository.java
@@ -0,0 +1,11 @@
+package io.github.scorpionsik.learn.LearnSpringInAction.repositiries;
+
+import io.github.scorpionsik.learn.LearnSpringInAction.models.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
